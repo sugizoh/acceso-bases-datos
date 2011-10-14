0d220b4647a6a10744f36f31ffeefce135ecac8d
miguelgonzalezgomez <miguelgonzalezgomez@gmail.com>
2011-10-14 13:48:46 +0000

    Creando objeto main

--- Acceso Bases Datos Heterogeneas/src/main/Main.java
+++ Acceso Bases Datos Heterogeneas/src/main/Main.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package main;
 
 /**
@@ -14,6 +10,6 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Main main = new Main();
     }
 }
