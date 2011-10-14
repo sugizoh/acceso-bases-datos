dce474af6c4900912221e62df74ab04ae316f1b9
jaimebarez <jaimebarez@gmail.com>
2011-10-14 13:55:13 +0000

    Subida! error?

--- Acceso Bases Datos Heterogeneas/src/main/Main.java
+++ Acceso Bases Datos Heterogeneas/src/main/Main.java
@@ -10,7 +10,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Main main = new Main();
+        Main main = new Main();//Cambio jaime
         //
     }
 }
