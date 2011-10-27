7dc97d386eab9073a500d88da20267bda20463e8
jaimebarez@gmail.com <jaimebarez@gmail.com@fadd4ef6-37bb-407c-4ab0-ce4ecf339d1a>
2011-10-27 21:31:43 +0000

    palabras reservadas

--- trunk/Acceso Bases Datos Heterogeneas/src/main/Diccionario.java
+++ trunk/Acceso Bases Datos Heterogeneas/src/main/Diccionario.java
@@ -11,7 +11,7 @@ public class Diccionario {
     //-------------------------
     //-----------REVISAR----------------
     //---------------------
-    private final String palabrasReservadas = " ¬,¬=¬>¬<¬(¬)¬\"¬*¬+¬-¬\\¬/¬SELECT¬FROM¬WHERE¬CHAT¬CONCAT¬FIELD¬FORMAT¬HEX¬INSERT¬INSTR¬LCASE¬LEFT¬LIKE¬LOCATE¬LOWER¬LPAD¬LTRIM¬MATCH¬MID¬NOT¬ORD¬POSITION¬QUOTE¬REPLACE¬REVERSE¬RIGHT¬RLIKE¬RPAD¬RTRIM¬SPACE¬STRCMP¬SUBSTR¬TRIM¬UCASE¬UPPER¬SUBSTRING¬COUNT¬ADD¬ALL¬ALTER¬ANALYZE¬AND¬AS¬ASC¬ASENSITIVE¬BEFORE¬BETWEEN¬BIGINT¬BINARY¬BLOB¬BOTH¬BY¬CALL¬CASCADE¬CASE¬CHANGE¬CHAR¬CHARACTER¬CHECK¬COLLATE¬COLUMN¬CONDITION¬CONSTRAINT¬CONTINUE¬CONVERT¬COUNT¬CREATE¬CROSS¬CURRENT_DATE¬CURRENT_TIME¬CURRENT_TIMESTAMP¬CURRENT_USER¬CURSOR¬DATABASE¬DATABASES¬DAY_HOUR¬DAY_MICROSECOND¬DAY_MINUTE¬DAY_SECOND¬DEC¬DECIMAL¬DECLARE¬DEFAULT¬DELAYED¬DELETE¬DESC¬DESCRIBE¬DETERMINISTIC¬DISTINCT¬DISTINCTROW¬DIV¬DOUBLE¬DROP¬DUAL¬EACH¬ELSE¬ELSEIF¬ENCLOSED¬ESCAPED¬EXISTS¬EXIT¬EXPLAIN¬FALSE¬FETCH¬FLOAT¬FLOAT4¬FLOAT8¬FOR¬FORCE¬FOREIGN¬FROM¬FULLTEXT¬GRANT¬GROUP¬HAVING¬HIGH_PRIORITY¬HOUR_MICROSECOND¬HOUR_MINUTE¬HOUR_SECOND¬IF¬IGNORE¬IN¬INDEX¬INFILE¬INNER¬INOUT¬INSENSITIVE¬INSERT¬INT¬INT1¬INT2¬INT3¬INT4¬INT8¬INTEGER¬INTERVAL¬INTO¬IS¬ITERATE¬JOIN¬KEY¬KEYS¬KILL¬LEADING¬LEAVE¬LEFT¬LIKE¬LIMIT¬LINES¬LOAD¬LOCALTIME¬LOCALTIMESTAMP¬LOCK¬LONG¬LONGBLOB¬LONGTEXT¬LOOP¬LOW_PRIORITY¬MATCH¬MEDIUMBLOB¬MEDIUMINT¬MEDIUMTEXT¬MIDDLEINT¬MINUTE_MICROSECOND¬MINUTE_SECOND¬MOD¬MODIFIES¬NATURAL¬NOT¬NO_WRITE_TO_BINLOG¬NULL¬NUMERIC¬ON¬OPTIMIZE¬OPTION¬OPTIONALLY¬OR¬ORDER¬OUT¬OUTER¬OUTFILE¬PRECISION¬PRIMARY¬PROCEDURE¬PURGE¬READ¬READS¬REAL¬REFERENCES¬REGEXP¬RELEASE¬RENAME¬REPEAT¬REPLACE¬REQUIRE¬RESTRICT¬RETURN¬REVOKE¬RIGHT¬RLIKE¬SCHEMA¬SCHEMAS¬SECOND_MICROSECOND¬SELECT¬SENSITIVE¬SEPARATOR¬SET¬SHOW¬SMALLINT¬SONAME¬SPATIAL¬SPECIFIC¬SQL¬SQLEXCEPTION¬SQLSTATE¬SQLWARNING¬SQL_BIG_RESULT¬SQL_CALC_FOUND_ROWS¬SQL_SMALL_RESULT¬SSL¬STARTING¬STRAIGHT_JOIN¬TABLE¬TERMINATED¬THEN¬TINYBLOB¬TINYINT¬TINYTEXT¬ TO ¬TRAILING¬TRIGGER¬TRUE¬UNDO¬UNION¬UNIQUE¬UNLOCK¬UNSIGNED¬UPDATE¬USAGE¬USE¬USING¬UTC_DATE¬UTC_TIME¬UTC_TIMESTAMP¬VALUES¬VARBINARY¬VARCHAR¬VARCHARACTER¬VARYING¬WHEN¬WHERE¬WHILE¬WITH¬WRITE¬XOR¬YEAR_MONTH¬ZEROFILL";
+    private final String palabrasReservadas = " ¬,¬=¬>¬<¬(¬)¬\"¬*¬+¬-¬\\¬/¬SELECT¬FROM¬WHERE¬CHAT¬CONCAT¬FIELD¬FORMAT¬HEX¬INSERT¬INSTR¬LCASE¬LEFT¬LIKE¬LOCATE¬LOWER¬LPAD¬LTRIM¬MATCH¬MID¬NOT¬ORD¬POSITION¬QUOTE¬REPLACE¬REVERSE¬RIGHT¬RLIKE¬RPAD¬RTRIM¬SPACE¬STRCMP¬SUBSTR¬TRIM¬UCASE¬UPPER¬SUBSTRING¬COUNT¬ADD¬ALL¬ALTER¬ANALYZE¬AND¬AS¬ASC¬ASENSITIVE¬BEFORE¬BETWEEN¬BIGINT¬BINARY¬BLOB¬BOTH¬BY¬CALL¬CASCADE¬CASE¬CHANGE¬CHAR¬CHARACTER¬CHECK¬COLLATE¬COLUMN¬CONDITION¬CONSTRAINT¬CONTINUE¬CONVERT¬COUNT¬CREATE¬CROSS¬CURRENT_DATE¬CURRENT_TIME¬CURRENT_TIMESTAMP¬CURRENT_USER¬CURSOR¬DATABASE¬DATABASES¬DAY_HOUR¬DAY_MICROSECOND¬DAY_MINUTE¬DAY_SECOND¬DEC¬DECIMAL¬DECLARE¬DEFAULT¬DELAYED¬DELETE¬DESC¬DESCRIBE¬DETERMINISTIC¬DISTINCT¬DISTINCTROW¬DIV¬DOUBLE¬DROP¬DUAL¬EACH¬ELSE¬ELSEIF¬ENCLOSED¬ESCAPED¬EXISTS¬EXIT¬EXPLAIN¬FALSE¬FETCH¬FLOAT¬FLOAT4¬FLOAT8¬FOR¬FORCE¬FOREIGN¬FROM¬FULLTEXT¬GRANT¬GROUP¬HAVING¬HIGH_PRIORITY¬HOUR_MICROSECOND¬HOUR_MINUTE¬HOUR_SECOND¬IF¬IGNORE¬IN¬INDEX¬INFILE¬INNER¬INOUT¬INSENSITIVE¬INSERT¬INT¬INT1¬INT2¬INT3¬INT4¬INT8¬INTEGER¬INTERVAL¬INTO¬IS¬ITERATE¬JOIN¬KEY¬KEYS¬KILL¬LEADING¬LEAVE¬LEFT¬LIKE¬LIMIT¬LINES¬LOAD¬LOCALTIME¬LOCALTIMESTAMP¬LOCK¬LONG¬LONGBLOB¬LONGTEXT¬LOOP¬LOW_PRIORITY¬MATCH¬MEDIUMBLOB¬MEDIUMINT¬MEDIUMTEXT¬MIDDLEINT¬MINUTE_MICROSECOND¬MINUTE_SECOND¬MOD¬MODIFIES¬NATURAL¬NOT¬NO_WRITE_TO_BINLOG¬NULL¬NUMERIC¬ON¬OPTIMIZE¬OPTION¬OPTIONALLY¬OR¬ORDER¬OUT¬OUTER¬OUTFILE¬PRECISION¬PRIMARY¬PROCEDURE¬PURGE¬READ¬READS¬REAL¬REFERENCES¬REGEXP¬RELEASE¬RENAME¬REPEAT¬REPLACE¬REQUIRE¬RESTRICT¬RETURN¬REVOKE¬RIGHT¬RLIKE¬SCHEMA¬SCHEMAS¬SECOND_MICROSECOND¬SELECT¬SENSITIVE¬SEPARATOR¬SET¬SHOW¬SMALLINT¬SONAME¬SPATIAL¬SPECIFIC¬SQL¬SQLEXCEPTION¬SQLSTATE¬SQLWARNING¬SQL_BIG_RESULT¬SQL_CALC_FOUND_ROWS¬SQL_SMALL_RESULT¬SSL¬STARTING¬STRAIGHT_JOIN¬TABLE¬TERMINATED¬THEN¬TINYBLOB¬TINYINT¬TINYTEXT¬TO¬TRAILING¬TRIGGER¬TRUE¬UNDO¬UNION¬UNIQUE¬UNLOCK¬UNSIGNED¬UPDATE¬USAGE¬USE¬USING¬UTC_DATE¬UTC_TIME¬UTC_TIMESTAMP¬VALUES¬VARBINARY¬VARCHAR¬VARCHARACTER¬VARYING¬WHEN¬WHERE¬WHILE¬WITH¬WRITE¬XOR¬YEAR_MONTH¬ZEROFILL";
     /*Se ordena el array de mayor a menor para evitar hacer interpretar como 
     no reservada por. ej. SUBSTR cuando el usuario ha escrito SUBSTRING 
     que es otra función equivalente de mysql, ya que el array al comparar
@@ -22,6 +22,24 @@ public class Diccionario {
      * @param miCadena 
      * @return int Número de letras de la palabra reservada por la que comienza, si no empieza por reservada, devuelve -1
      */
+    
+    private final String palabrasSeparadoras = " ¬,¬=¬>¬<¬(¬)¬\"¬*¬+¬-¬\\¬/";
+    private final String arraySeparadoras[] = palabrasSeparadoras.split("¬");
+    
+    int esSeparadora(String miCadena) {
+        //Presuponemos que no es separadora
+        int es = -1;
+        
+        //Comparamos nuestra cadena a ver si empieza con alguna de las palabras separadoras
+        for (int i=0; i<arraySeparadoras.length && es==-1; i++) {
+            if(miCadena.startsWith(arraySeparadoras[i]))
+            {
+                es = arraySeparadoras[i].length();
+            }
+        }
+        return es;
+    }
+    
     int esReservada(String miCadena) {
         //Trabajaremos con una cadena local, que es la recibida pasada a mayúsculas(ya que no importa la capitalización de las letras)
         String cadenaLocal = miCadena.toUpperCase();
@@ -34,13 +52,15 @@ public class Diccionario {
         
         //Comparamos nuestra cadena a ver si empieza con alguna de las palabras reservadas
         for (int i=0; i<arrayReservadas.length && es==-1; i++) {
-            if(cadenaLocal.startsWith(arrayReservadas[i]))
+            if(cadenaLocal.equals(arrayReservadas[i]))
             {
+                System.out.println("comparando " + cadenaLocal + "con" +  arrayReservadas[i] + ".");
                 es = arrayReservadas[i].length();
             }
         }
         return es;
     }
+    
     
     /**
      * Desmiembra una cadena en un ArrayList de dos columnas: 
@@ -56,38 +76,45 @@ public class Diccionario {
         ArrayList miArrayList = new ArrayList();
         
         String subCadena= new String();
-        int longPalabraReservada=0;
+        int longPalabraSeparadora=0;
         
         //Auxiliar que usamos para ir guardando palabras no reservadas
-        String noReservada = "";
+        String noSeparadora = "";
+        
+        //Auxiliar booleano
+        Boolean separadorOReservada=false;
         
         
         for (int i=0; i<longMiCadena; i++) {
             //Vamos analizando la cadena mediante una subcadena que cada vez se hace más pequeña
             subCadena = miCadena.substring(i, longMiCadena);
             //Longitud de la palabra reservada al principio de la cadena. Si no la hay, es -1
-            longPalabraReservada = esReservada(subCadena);
+            longPalabraSeparadora = esSeparadora(subCadena);
             
             //Si no hay una palabra reservada al principio
-            if(longPalabraReservada<=0) {
+            if(longPalabraSeparadora<=0) {
                 //Añadimos la primera letra de la cadena con la que trabajamos a nuestro prototipo de palabra no reservada
-                noReservada = noReservada.concat(Character.toString(subCadena.charAt(0)));
+                noSeparadora = noSeparadora.concat(Character.toString(subCadena.charAt(0)));
                 //Si hemos llegado al final de la cadena inicial, guardamos nuestra palabra no reservada
                 if(i==longMiCadena-1){
-                    miArrayList.add(new tuplaPalabra(noReservada, false));
+                    if(esReservada(noSeparadora)>0) {separadorOReservada=true;}
+                    else{separadorOReservada=false;}
+                    miArrayList.add(new tuplaPalabra(noSeparadora, separadorOReservada));
                 }
                 
             }
             //Si la primera palabra de la cadena que estamos analizando es palabra reservada
             else {
                 //Comprobamos que no existiera una no reservada de antes sin guardar. Si existe la guardamos y vaciamos nuestra palabra auxiliar
-                if(!noReservada.equalsIgnoreCase("")) {
-                    miArrayList.add(new tuplaPalabra(noReservada, false));
-                    noReservada="";
+                if(!noSeparadora.equalsIgnoreCase("")) {
+                    if(esReservada(noSeparadora)>0) {separadorOReservada=true;}
+                    else{separadorOReservada=false;}
+                    miArrayList.add(new tuplaPalabra(noSeparadora, separadorOReservada));
+                    noSeparadora="";
                 }
                 //Añadimos al arraylist nuestra palabra reservada
-                miArrayList.add(new tuplaPalabra(miCadena.substring(i, i + longPalabraReservada), true));
-                i = i + longPalabraReservada-1;
+                miArrayList.add(new tuplaPalabra(miCadena.substring(i, i + longPalabraSeparadora), true));
+                i = i + longPalabraSeparadora-1;
             }
             
         }
