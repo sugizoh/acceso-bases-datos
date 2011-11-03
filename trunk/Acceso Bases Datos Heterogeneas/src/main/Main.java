8ad8e8314adbb84270a84e17f37468a7939df630
miguelgonzalezgomez@gmail.com <miguelgonzalezgomez@gmail.com@fadd4ef6-37bb-407c-4ab0-ce4ecf339d1a>
2011-11-03 17:52:24 +0000

    Ideas

--- trunk/Acceso Bases Datos Heterogeneas/src/main/Main.java
+++ trunk/Acceso Bases Datos Heterogeneas/src/main/Main.java
@@ -28,17 +28,32 @@ public class Main {
     Traductor traductor;
 
     public Main() {
-        
         traductor = new Traductor();
         lectura = new LecturaTeclado();
         
         System.out.println("Escribe la consulta SQL:");
+        //SELECT DE PRUEBA, por ej.
+        //SELECT idLibro, titulo, ISBN, paginas, stock, fechaEdicion FROM Libro;
         String consultaSQL = lectura.leetTexto();
 
         ArrayList<String> traducciones = traductor.getConsultasTraducidas(consultaSQL);
 
         for(int i=0; i<traducciones.size(); i++)
             System.out.println(traducciones.get(i));
+
+        /*
+         * Ahora faltaría crear una clase BD [la cula emplea la clase Configuracion para realizar las conexiones, y segun el tipo de conexion
+         * tendria que ejecutarle un sql del traductor u otro [traducciones pues cambiar que devuelva un hashmap (clave => basedatos, valor => sql) por ej.
+         *
+         * Analizar si meter el traductor dentro de bd o en el main mandando las traducciones a la bd.
+         *
+         * Despues de esto ya mirar patrones y mierdas para no violar nada y cerrar cualquier bug. y listo, documentar funciones
+         * hacer junits y mierdas, y mandarlo a mariano y a por la siguiente iteracion
+         *
+         * [usar regex? analizar, si no ya empezar con version grafica sencilla por ej.]
+         *
+         * ¿Que te parece jaime? Estas son mis ideas que he sacado a lo largo del día
+         */
     }
     /**
      * @param args the command line arguments
