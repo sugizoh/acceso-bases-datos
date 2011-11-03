37ea51616017e7fc3b7fb7faff77ce32d9cb6ff4
miguelgonzalezgomez <miguelgonzalezgomez@gmail.com>
2011-11-03 18:14:54 +0000

    Limpieza cabeceras

--- Acceso Bases Datos Heterogeneas/src/main/Main.java
+++ Acceso Bases Datos Heterogeneas/src/main/Main.java
@@ -1,21 +1,6 @@
 package main;
-import Utilidades.Tupla;
-import Utilidades.Analizador;
-import BD.BaseDatos_old;
 import Utilidades.LecturaTeclado;
-import XML.Configuracion;
-import XML.Diccionario;
-import java.sql.Statement;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
