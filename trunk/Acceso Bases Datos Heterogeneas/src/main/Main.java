74094f1abfb8f8bc2dd01045246e442aca7f1fc0
jaimebarez@gmail.com <jaimebarez@gmail.com@fadd4ef6-37bb-407c-4ab0-ce4ecf339d1a>
2011-11-07 15:46:49 +0000

    subida

--- trunk/Acceso Bases Datos Heterogeneas/src/main/Main.java
+++ trunk/Acceso Bases Datos Heterogeneas/src/main/Main.java
@@ -1,4 +1,5 @@
 package main;
+import BD.Consultor;
 import Utilidades.LecturaTeclado;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -23,22 +24,8 @@ public class Main {
         String consultaSQL = lectura.leerTexto();
 
         HashMap<String, String> traducciones = traductor.getConsultasTraducidas(consultaSQL);
-
+        Consultor consultor = new Consultor();
         
-
-        /*
-         * Ahora faltaría crear una clase BD [la cula emplea la clase Configuracion para realizar las conexiones, y segun el tipo de conexion
-         * tendria que ejecutarle un sql del traductor u otro [traducciones pues cambiar que devuelva un hashmap (clave => basedatos, valor => sql) por ej.
-         *
-         * Analizar si meter el traductor dentro de bd o en el main mandando las traducciones a la bd.
-         *
-         * Despues de esto ya mirar patrones y mierdas para no violar nada y cerrar cualquier bug. y listo, documentar funciones
-         * hacer junits y mierdas, y mandarlo a mariano y a por la siguiente iteracion
-         *
-         * [usar regex? analizar, si no ya empezar con version grafica sencilla por ej.]
-         *
-         * ¿Que te parece jaime? Estas son mis ideas que he sacado a lo largo del día
-         */
     }
     /**
      * @param args the command line arguments
