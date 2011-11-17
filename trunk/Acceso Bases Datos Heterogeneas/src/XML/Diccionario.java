94fbfcd095bee324548940c73a83cc3bb440decc
jaimebarez@gmail.com <jaimebarez@gmail.com@fadd4ef6-37bb-407c-4ab0-ce4ecf339d1a>
2011-11-17 07:25:03 +0000

    comments

--- trunk/Acceso Bases Datos Heterogeneas/src/XML/Diccionario.java
+++ trunk/Acceso Bases Datos Heterogeneas/src/XML/Diccionario.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package XML;
 
 import Utilidades.XML;
@@ -14,6 +9,7 @@ import java.util.Map;
 /**
  *
  * @author Miguel González y Jaime Bárez
+ * Clase encargada del diccionario
  */
 public class Diccionario extends XML
 {
--- trunk/Acceso Bases Datos Heterogeneas/src/main/Main.java
+++ trunk/Acceso Bases Datos Heterogeneas/src/main/Main.java
@@ -1,4 +1,5 @@
 package main;
+
 import BD.Consultor;
 import Utilidades.LecturaTeclado;
 import java.util.ArrayList;
@@ -6,31 +7,36 @@ import java.util.HashMap;
 
 /**
  *
- * @author Jaime Bárez y Miguel González
+ * @author Miguel González y Jaime Bárez
  * Clase principal del programa
  */
 public class Main {
 
     LecturaTeclado lectura;
     Traductor traductor;
+    Consultor consultor;
 
     public Main() {
         traductor = new Traductor();
         lectura = new LecturaTeclado();
+        consultor = new Consultor();
         
         System.out.println("Escribe la consulta SQL:");
         //SELECT DE PRUEBA, por ej.
         //SELECT idLibro, titulo, ISBN, paginas, stock, fechaEdicion FROM Libro;
         String consultaSQL = lectura.leerTexto();
 
+        //El primer string nos indica la base de de datos a la que pertenece la consulta. El segundo la consulta ya traducida.
         HashMap<String, String> traducciones = traductor.getConsultasTraducidas(consultaSQL);
-        Consultor consultor = new Consultor();
+        
+        //ArrayList con las respuestas del servidor a las consultas lanzadas
         ArrayList<String> resultados = consultor.lanzarConsulta(traducciones);
 
         for(int i=0; i<resultados.size(); i++)
             System.out.println(resultados.get(i));
     }
     /**
+     * Función principal del progama
      * @param args the command line arguments
      */
     public static void main(String[] args) {
