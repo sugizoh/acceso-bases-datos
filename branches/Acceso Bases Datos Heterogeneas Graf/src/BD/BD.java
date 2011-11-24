fd71ceefb79dad88184816398496cad7e51f7d8e
miguelgonzalezgomez@gmail.com <miguelgonzalezgomez@gmail.com@fadd4ef6-37bb-407c-4ab0-ce4ecf339d1a>
2011-11-24 07:31:33 +0000

    Modificado, quitazo try...catch y se lanzan hacia arriba los errores

--- branches/Acceso Bases Datos Heterogeneas Graf/src/BD/BD.java
+++ branches/Acceso Bases Datos Heterogeneas Graf/src/BD/BD.java
@@ -5,8 +5,6 @@
 package BD;
 
 import java.sql.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Clase que maneja las operaciones de bases de datos
@@ -18,47 +16,24 @@ public class BD {
     /**
      * Constructor
      */
-    public BD() {
-        try {
-            // Se registra el Driver de MySQL
-            DriverManager.registerDriver(new org.gjt.mm.mysql.Driver());
-        } catch (SQLException ex) {
-            Logger.getLogger(BD.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
+    public BD() throws SQLException{
+        // Se registra el Driver de MySQL
+        DriverManager.registerDriver(new org.gjt.mm.mysql.Driver());
     }
     
-    public void abrirBD(String cadenaConexion, String usuario, String contraseña) {
-        try {
-            // Se obtiene una conexión con la base de datos..
-            conexion = DriverManager.getConnection (
-            cadenaConexion,usuario, contraseña);
+    public void abrirBD(String cadenaConexion, String usuario, String contraseña) throws SQLException {
+        // Se obtiene una conexión con la base de datos..
+        conexion = DriverManager.getConnection (cadenaConexion,usuario, contraseña);
 
-            // Se crea un Statement, para realizar la consulta
-            myStatement = conexion.createStatement();
-        } catch (SQLException ex) {
-            Logger.getLogger(BD.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
+        // Se crea un Statement, para realizar la consulta
+        myStatement = conexion.createStatement();
     }
     
-    public ResultSet consultar(String consulta) {
-        ResultSet rs = null;
-        try {
-            // Se realiza la consulta. Los resultados se guardan en el
-            // ResultSet rs
-            rs = myStatement.executeQuery (consulta);
-        } catch (SQLException ex) {
-            Logger.getLogger(BD.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return rs;
+    public ResultSet consultar(String consulta) throws SQLException {
+        return myStatement.executeQuery (consulta);
     }
     
-    public void cerrar() {
-        try {
-            conexion.close();
-        } catch (SQLException ex) {
-            Logger.getLogger(BD.class.getName()).log(Level.SEVERE, null, ex);
-        }
+    public void cerrar() throws SQLException {
+        conexion.close();
     }
 }
