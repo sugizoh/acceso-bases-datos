d0d4689d1fb7f4b5d85fc94015807146eaba1b1a
jaimebarez <jaimebarez@gmail.com>
2011-11-28 09:16:59 +0000

    cambios

--- test/BD/BDTest.java
+++ test/BD/BDTest.java
@@ -2,10 +2,12 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package BD;
 
+import java.sql.Statement;
+import java.sql.Connection;
 import java.sql.DriverManager;
-import com.mysql.jdbc.Statement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import org.junit.After;
@@ -17,26 +19,28 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author JaimeInves
+ * @author Miguel González y Jaime Bárez
  */
 public class BDTest {
-    
-    public BDTest() {
+
+    public BDTest() throws SQLException {
+        // Se registra el Driver de MySQL
+        DriverManager.registerDriver(new org.gjt.mm.mysql.Driver());
     }
 
     @BeforeClass
     public static void setUpClass() throws Exception {
-        
     }
 
+    //Cerramos la conexión una vez terminada las pruebas
     @AfterClass
     public static void tearDownClass() throws Exception {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -45,15 +49,25 @@ public class BDTest {
      * Test of abrirBD method, of class BD.
      */
     @Test
-    public void testAbrirBD() throws Exception {
-        System.out.println("abrirBD");
-        String cadenaConexion = "jdbc:mysql://localhost/amazon";
+    public void testAbrirCerrarConexiones() throws Exception {
+        //Creamos un objeto BD
+        BD instance = new BD();
+
+        System.out.println("Abriendo conexión con Amazon");
+        String cadenaConexion = "jdbc:mysql://localhost/Amazon";
         String usuario = "root";
         String contraseña = "sql";
-        BD instance = new BD();
         instance.abrirBD(cadenaConexion, usuario, contraseña);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+
+        System.out.println("Cerrando conexión con amazon");
+        instance.cerrar();
+
+        System.out.println("Abriendo conexión con CasaDelLibro");
+        cadenaConexion = "jdbc:mysql://localhost/CasaDelLibro";
+        instance.abrirBD(cadenaConexion, usuario, contraseña);
+
+        System.out.println("Cerrando conexión con CasaDelLibro");
+        instance.cerrar();
     }
 
     /**
@@ -61,32 +75,94 @@ public class BDTest {
      */
     @Test
     public void testConsultar() throws Exception {
-        System.out.println("consultar");
-        String consulta = "SELECT substr(titulo,1,10), fechaEdicion, precio, nombreAutor FROM Libro, Autor WHERE Libro.idAutor = Autor.idAutor";
         BD instance = new BD();
-        ResultSet expResult = null;
-        ResultSet result = instance.consultar(consulta);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
-    }
-    
-    
-    public ResultSet resultadoConsulta(String consulta) throws SQLException {
-        Statement myStatement = null;
-        return myStatement.executeQuery (consulta);
-    }
 
-    /**
-     * Test of cerrar method, of class BD.
-     */
-    @Test
-    public void testCerrar() throws Exception {
-        System.out.println("cerrar");
-        BD instance = new BD();
-        instance.abrirBD("jdbc:mysql://localhost/amazon", "root", "sql");
+        System.out.println("Comprobando consulta a Amazon");
+
+
+
+        //Abrimos la conexión con la base de datos
+        String cadenaConexion = "jdbc:mysql://localhost/Amazon";
+        String usuario = "root";
+        String contraseña = "sql";
+        instance.abrirBD(cadenaConexion, usuario, contraseña);
+
+        //Consulta SQL
+        String consulta = "SELECT tituloLibro, editorial, numPaginas, nombreAutor FROM Amazon, Autor WHERE Amazon.idAutor = Autor.idAutor;";
+
+        Connection conexion = DriverManager.getConnection (cadenaConexion,usuario, contraseña);
+        Statement myStatement = conexion.createStatement();
+
+        ResultSet expResult = myStatement.executeQuery (consulta); //ResultSet experado
+        ResultSet result = instance.consultar(consulta); //ResultSet devuelto por BD
+
+        //Comparamos los resultados
+        if(expResult.getMetaData().getColumnCount() == result.getMetaData().getColumnCount())
+        {
+            int numColumnas = result.getMetaData().getColumnCount();
+
+            while (result.next()) {
+                if(expResult.next()) {
+                for(int j=1; j<=numColumnas; j++) {
+                    String valor = result.getString(j).toString();
+                    String valorExp = expResult.getString(j).toString();
+
+                    if(!valor.equals(valorExp)) {
+                        fail("La consulta devuelve valores distintos");
+                    }
+                }
+                } else {
+                    fail("La consulta a Amazon devuelve distinto número de filas");
+                }
+            }
+        } else {
+            fail("La consulta a Amazon devuelve distinto número de columnas");
+        }
+
+        //Cerramos la conexión con la base de datos
         instance.cerrar();
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+        conexion.close();
+
+        System.out.println("Comprobando consulta a CasaDelLibro");
+        
+        //Abrimos la conexión con CasaDelLibro
+        cadenaConexion = "jdbc:mysql://localhost/CasaDelLibro";
+        instance.abrirBD(cadenaConexion, usuario, contraseña);
+
+        //Consulta SQL
+        consulta = "SELECT titulo, ISBN, fechaEdicion, nombreAutor FROM CasaDelLibro, Autor WHERE CasaDelLibro.idAutor = Autor.idAutor;";
+        
+        conexion = DriverManager.getConnection (cadenaConexion,usuario, contraseña);
+        myStatement = conexion.createStatement();
+
+        expResult = myStatement.executeQuery (consulta); //ResultSet experado
+        result = instance.consultar(consulta); //ResultSet devuelto por BD
+
+        //Comparamos los resultados
+        if(expResult.getMetaData().getColumnCount() == result.getMetaData().getColumnCount())
+        {
+            int numColumnas = result.getMetaData().getColumnCount();
+
+            while (result.next()) {
+                if(expResult.next()) {
+                for(int j=1; j<=numColumnas; j++) {
+                    String valor = result.getString(j).toString();
+                    String valorExp = expResult.getString(j).toString();
+
+                    if(!valor.equals(valorExp)) {
+                        fail("La consulta devuelve valores distintos");
+                    }
+                }
+                } else {
+                    fail("La consulta a Amazon devuelve distinto número de filas");
+                }
+            }
+        } else {
+            fail("La consulta a Amazon devuelve distinto número de columnas");
+        }
+        
+        //Cerramos la conexión con la base de datos
+        instance.cerrar();
+        conexion.close();
     }
 }
--- test/BD/ConsultorTest.java
+++ test/BD/ConsultorTest.java
@@ -5,8 +5,13 @@
 package BD;
 
 import GUI.ModeloTabla;
-import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -20,11 +25,24 @@ import static org.junit.Assert.*;
  */
 public class ConsultorTest {
     
-    public ConsultorTest() {
+   
+    private Statement myStatement;
+    public String cadenaConexionAmazon = "jdbc:mysql://localhost/Amazon";
+    public String cadenaConexionCasaDelLibro = "jdbc:mysql://localhost/CasaDelLibro";
+    public String[] cadenaConexiones= {cadenaConexionAmazon, cadenaConexionCasaDelLibro};
+    public String usuario = "root";
+    public String contraseña = "sql";
+    private Connection conexion;
+    
+    public ConsultorTest() throws SQLException {
+        DriverManager.registerDriver(new org.gjt.mm.mysql.Driver());
+        System.out.println("Abriendo conexión con Amazon");
+        
     }
 
     @BeforeClass
     public static void setUpClass() throws Exception {
+        
     }
 
     @AfterClass
@@ -43,36 +61,86 @@ public class ConsultorTest {
      * Test of lanzarConsulta method, of class Consultor.
      */
     @Test
-    public void testLanzarConsulta() {
+    public void testLanzarConsulta() throws Exception {
         System.out.println("lanzarConsulta");
-        HashMap<String, String> sentenciasSQL = null;
-        
-        String consultaPrueba = new String();
-        consultaPrueba = "SELECT substr(titulo,1,10), fechaEdicion, precio, nombreAutor FROM Amazon, Autor WHERE Amazon.idAutor = Autor.idAutor";
-        sentenciasSQL.put("amazon", consultaPrueba);
-        
-        consultaPrueba = "SELECT substr(titulo,1,10), precio, nombreAutor FROM CasaDelLibro, Autor WHERE CasaDelLibro.idAutor = Autor.idAutor";
-        sentenciasSQL.put("casadellibro", consultaPrueba);
-        
         Consultor instance = new Consultor();
-        ModeloTabla expResult = null;
+        HashMap<String, String> sentenciasSQL = new HashMap<String, String>();
+        
+        
+        String [] nombresBasesDatos= {"amazon", "casadellibro"};
+        int numBaseDatos = 2;
+        
+        sentenciasSQL.put("amazon", "SELECT substr(tituloLibro,1,10), fechaEdicion, precio, nombreAutor FROM Amazon, Autor WHERE Amazon.idAutor = Autor.idAutor");
+        sentenciasSQL.put("casadellibro", "SELECT substr(titulo,1,10), precio, nombreAutor FROM CasaDelLibro, Autor WHERE CasaDelLibro.idAutor = Autor.idAutor");
+
+        
+        
+        
+        String headTable[] = null;
+        String datos[][] = null;
+        int numColumnas = 0;
+        ArrayList<String[]> arrayDatos = new ArrayList<String[]>();
+        
+        for(int i=0; i<numBaseDatos; i++) {
+            String nombreBD = nombresBasesDatos[i];
+            conexion = DriverManager.getConnection (cadenaConexiones[i],usuario, contraseña);
+            myStatement = conexion.createStatement();
+            ResultSet rsConsulta = myStatement.executeQuery (sentenciasSQL.get(nombreBD));
+            
+            numColumnas = rsConsulta.getMetaData().getColumnCount();
+
+            //En la primera base de datos obtenemos los datos de la cabecera de la tabla
+            if(i == 0) {
+                headTable = new String[numColumnas];
+                for(int j=0; j<numColumnas; j++) {
+                    headTable[j] = new String();
+                }
+            }
+
+            //Traducimos la headTable
+            instance.completarTraduccionInversaNombresColumnas(headTable, rsConsulta);
+
+            int consultaActual = 0;
+            while (rsConsulta.next()) {
+                String []filaDatos = new String[numColumnas];
+                for(int j=1; j<=numColumnas; j++) {
+                    String valor = rsConsulta.getString(j).toString();
+                    filaDatos[j-1] = valor;
+                }
+                arrayDatos.add(filaDatos);
+                consultaActual++;
+            }
+            //Cerramos la conexión de la base de datos
+            conexion.close();
+        }
+
+        datos = new String[arrayDatos.size()][numColumnas];
+
+        for(int i=0; i<arrayDatos.size(); i++)
+            datos[i] = arrayDatos.get(i);
+
+
+        
+        
+        
+        ModeloTabla expResult = new ModeloTabla(datos,headTable);
         ModeloTabla result = instance.lanzarConsulta(sentenciasSQL);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //fail("The test case is a prototype.");
     }
 
     /**
      * Test of completarTraduccionInversaNombresColumnas method, of class Consultor.
      */
     @Test
-    public void testCompletarTraduccionInversaNombresColumnas() {
+    public void testCompletarTraduccionInversaNombresColumnas() throws Exception {
         System.out.println("completarTraduccionInversaNombresColumnas");
         String[] headTable = null;
         ResultSet rsConsulta = null;
         Consultor instance = new Consultor();
         instance.completarTraduccionInversaNombresColumnas(headTable, rsConsulta);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //fail("The test case is a prototype.");
     }
 }
